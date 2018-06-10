2955554a0e3ef5d58f779117b1ad4f75235f334d
Shu <sryhan8520@gmail.com>
2018-06-10 18:27:07 -0400

    new stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch');
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
 
 gulp.task('default', function() {
     console.log("You created a gulp task");
@@ -10,7 +15,9 @@ gulp.task("html", function() {
 });
 
 gulp.task("styles", function() {
-    console.log("Imagine SASS or PostCSS tasks running here");
+    return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles/'));
 });
 
 gulp.task("watch", function() {
