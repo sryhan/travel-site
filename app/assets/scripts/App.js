e04b3304cb2f0a5bb9ea0d1df1cc89ec4873bf35
Shuvo Ryhan <shuvo.ryhan@ntconcepts.com>
2018-07-07 16:32:27 -0400

    added reveal on scroll behavior

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,7 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnSCroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu(); 
+new RevealOnSCroll($(".feature-item"), "85%");
+new RevealOnSCroll($(".testimonial"), "60%");
