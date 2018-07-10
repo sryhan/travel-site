84f97c8f4a8282d446fa83f4ea82c1ed998bbcca
Shuvo Ryhan <shuvo.ryhan@ntconcepts.com>
2018-07-09 20:06:28 -0400

    completed sticy header and smooth scrolling

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnSCroll from './modules/RevealOnScroll';
 import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
 
 var mobileMenu = new MobileMenu(); 
 new RevealOnSCroll($(".feature-item"), "85%");
 new RevealOnSCroll($(".testimonial"), "60%");
+var stickyHeader = new StickyHeader();
